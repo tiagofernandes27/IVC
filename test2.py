b77b3e881ad14ee254fde54e3424885a681bd0b1
Diogo Silva <diogovicente10@hotmail.com>
2022-12-02 17:53:42 +0000

    Phase 2 code (test2)

--- test2.py
+++ test2.py
@@ -6,17 +6,29 @@ cap = cv2.VideoCapture(0)
 
 ret, frame = cap.read()
 
+frame = frame[:, ::-1, :]
+
 BB = cv2.selectROI(frame, False)
 tracker.init(frame, BB)
 
 while True:
+    old_position = BB[0]
+
     ret, frame = cap.read()
     track_success, BB = tracker.update(frame)
     if track_success:
         top_left = (int(BB[0]), int(BB[1]))
         bottom_right = (int(BB[0]+BB[2]), int(BB[1]+BB[3]))
         cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 5)
+    frame = frame[:, ::-1, :]
     cv2.imshow('Output', frame)
+
+    if old_position < BB[0]:
+        print("left")
+
+    if old_position > BB[0]:
+        print("right")
+
     key = cv2.waitKey(1) & 0xff
     if key == ord('q'):
         break
